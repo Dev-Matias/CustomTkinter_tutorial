8633e63cf180d67bd69eeb9ab7be591d33cc7f32
Dev-Matias <devmatias01@gmail.com>
2025-05-27 21:48:38 -0300

    mini_proyects

--- ejercicios/ejercicio_1.py
+++
@@ -1,9 +0,0 @@
-import customtkinter as ctk
-
-app = ctk.CTk()
-app.title("Ejercicio 1") # titulo de la ventana
-app.geometry("600x400") # geometria de la ventana, tamaño de la ventana
-
-
-app.mainloop() # bucle principal de la aplicacion
-# el bucle principal de la aplicacion es el encargado de mantener la ventana abierta y de gestionar los eventos que ocurren en la ventana
